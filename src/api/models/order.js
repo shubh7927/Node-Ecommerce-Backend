aad6d4d88647a26f7f9692eb10fdd1aa5da97a1a
shubh7927 <shubhjaincodes@gmail.com>
2022-10-09 16:33:04 +0530

    Updated the Order Model

--- src/api/models/order.js
+++ src/api/models/order.js
@@ -1,81 +1,21 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-    customerDetail: {
-        address: {
-            type: String,
-            required: true
-        },
-        city: {
-            type: String,
-            required: true
-        },
-        state: {
-            type: String,
-            required: true
-        },
-        pincode: {
-            type: Number,
-            required: true,
-        },
-        phoneNumber: {
-            type: Number,
-            required: true
-        }
-    },
     orderItems: [
         {
-            name: {
-                type: String,
-                required: true
-            },
-            price: {
-                type: Number,
-                required: true
-            },
-            quantity: {
-                type: Number,
-                required: true
-            },
             product: {
                 type: mongoose.Schema.ObjectId,
                 ref: "Product",
                 required: true
             },
-            
+            quantity: Number
         }
     ],
     orderedBy: {
         type: mongoose.Schema.ObjectId,
         ref: "User"
     },
-    payment: {
-        id: {
-            type: String,
-            // required: true
-        },
-        status: {
-            type: String,
-            // required: true
-        }
-    },
-    paidAt: {
-        type: Date,
-        // required: true
-    },
-    itemsPrice: {
-        type: Number,
-        // required: true,
-    },
-    shippingPrice: {
-        type: Number,
-        // required: true
-    },
-    totalPrice: {
-        type: Number,
-        // required: true
-    },
-    orderStatus: {
+    status: {
         type: String,
         default: "Pending"
     },
